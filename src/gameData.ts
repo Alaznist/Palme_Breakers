03a0f6fca98302547339a9e91f05f5612639f793
Tinodeis <Netrixion@orange.fr>
2022-12-02 00:54:54 +0100

    gameData update

--- src/gameData.ts
+++ src/gameData.ts
@@ -5,26 +5,35 @@ class pays{
     nombreContamine: number;
     nombreSain: number;
     nombreMort: number;
-    tauxSoin: number; // Dépend de la corpo des médecins
     tauxContagion: number; // Si, sain, pourcentage d'être contaminé
     tauxMortalité: number; // Si infecté, faculté à mourir
 
     public constructor(nom: string, nombrePopulation: number, nombreContamine: number, nombreSain: number,
-        nombreMort: number,tauxSoin: number,tauxContagion: number,tauxMortalité: number){
+        nombreMort: number,tauxContagion: number,tauxMortalité: number){
             this.nom = nom;
             this.nombrePopulation = nombrePopulation;
             this.nombreContamine = nombreContamine;
             this.nombreSain = nombreSain;
             this.nombreMort = nombreMort;
-            this.tauxSoin = tauxSoin; // Dépend de la corpo des médecins
             this.tauxContagion = tauxContagion; // Si, sain, pourcentage d'être contaminé
             this.tauxMortalité = tauxMortalité; // Si infecté, faculté à mourir
+    }
+
+    // retourne les différences, en premier la diff de gens sains, en second les nouveaux contaminés, en troisième les morts
+    public updatePas(): number[]{
+        let diffConta = parseInt((this.nombreContamine * this.tauxContagion / 10000).toFixed(0));
+        this.nombreContamine += diffConta;
+        let diffMmort = parseInt((this.tauxMortalité * this.nombreContamine / 100).toFixed(0));
+        this.nombreMort += diffMmort;
+        this.nombreSain -= diffConta;
+        return [-diffConta, diffConta, diffMmort];
     }
 }
 
 
 class monde{
-    deltaTauxSoin: number; // à chaque pas de temps, le taux de soin des pays, augmente du delta
+    deltaTauxSoin: number; // à chaque pas de temps, le taux de soin des pays augmente du delta
+    tauxSoin : number = 0;
     nombrePopulation: number;
     nombreContamine: number;
     nombreSain: number;
@@ -39,6 +48,31 @@ class monde{
             this.nombreSain = nombreSain;
             this.nombreMort = nombreMort;
             this.listePays = listePays;
-        }
+    }
 
+    public pasTemp(){
+        this.pasSoin();
+        let diff: number[] = this.pasPopu();
+        this.nombreSain += diff[0];
+        this.nombreContamine += diff[1];
+        this.nombreMort += diff[2];
+    }
+
+    public pasSoin(){
+        this.tauxSoin += this.deltaTauxSoin;
+        if (this.tauxSoin >= 1000){
+            // App close
+        }
+    }
+
+    public pasPopu(): number[]{
+        let diff : number[] = [0, 0, 0];
+        this.listePays.forEach(pays => {
+            let temp : number[] = pays.updatePas();
+            for (let i in temp){
+                diff[i] += temp[i];
+            }
+        })
+        return diff;
+    }
 }

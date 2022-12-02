97b7c03d4fbbaced2e845892657ab6bd057c5a96
Tinodeis <Netrixion@orange.fr>
2022-12-02 03:06:16 +0100

    DataFinshed ?

--- src/tree.ts
+++ src/tree.ts
@@ -6,60 +6,9 @@ class Competence{
         this.effets = effects;
     }
 
-    public doCompet(){
-        // var modif here
-    }
-
     name : String;
     description : String;
     effets : number[];
-}
-
-class Noeux{
-
-    public constructor(countCompetence : number, competence : Competence, to : Noeux[]){
-        this.coutCompetence = countCompetence;
-        this.competance = competence;
-        this.to = to;
-    }
-
-    public unlockNoeux() {
-        if (!this.unlocked){
-            this.unlocked = true;
-        }
-    }
-
-    public unlockTo(){
-        this.to.forEach(element => {
-            element.unlockNoeux();
-        });
-    }
-
-    // fonc a appeler pour acheter un noeux
-    public buyNoeux(){
-        if (this.unlocked && !this.buyed /* && money > this.coutCompetence */){
-            // Link here money actions
-            this.competance.doCompet();
-            this.unlockTo()
-        }
-    }
-
-    coutCompetence : number;
-    unlocked : boolean = false;
-    buyed : boolean = false;
-    competance : Competence;
-    to : Noeux[];
-}
-
-class Arbre{
-
-    public constructor(name : String, noeux : Noeux){
-        this.name = name;
-        this.baseNoeux = noeux;
-    }
-
-    name : String;
-    baseNoeux : Noeux;
 }
 
 

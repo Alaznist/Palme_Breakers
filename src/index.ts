8b71ec98fb59a4f0783f3a17a9103ec66f289d9b
Pykorm <56791498+Pykorm@users.noreply.github.com>
2022-12-02 03:42:33 +0100

    ajout de truc graphique dans index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import { Graphics } from 'pixi.js';
 import { HelloWorld } from './scenes/helloWorld';
 
 const load = (app: PIXI.Application) => {
@@ -50,6 +51,44 @@ const main = async () => {
     // Load assets
     await load(app);
     document.body.appendChild(app.view);
+
+    // Create the sprite and add it to the stage
+    let sprite = PIXI.Sprite.from('assets/monde.webp');
+    app.stage.addChild(sprite);
+
+    let barre_fond = new Graphics();
+    barre_fond.beginFill(0);
+    barre_fond.drawRect(window.innerWidth * 0.1, window.innerHeight * 0.925, window.innerWidth * 0.8 ,  window.innerHeight * 0.025)
+    app.stage.addChild(barre_fond)
+
+    let progress_contaminee = 0.8;
+    let barre_caca = new Graphics();
+    barre_caca.beginFill(0x00FF00);
+    barre_caca.drawRect(window.innerWidth * 0.1, window.innerHeight * 0.925, window.innerWidth * (progress_contaminee * 0.8) ,  window.innerHeight * 0.025)
+    app.stage.addChild(barre_caca)
+
+    let progress_morte = 0.5;
+    let barre_prout = new Graphics();
+    barre_prout.beginFill(0x0000FF);
+    barre_prout.drawRect(window.innerWidth * 0.1, window.innerHeight * 0.925, window.innerWidth * (progress_morte * 0.8) ,  window.innerHeight * 0.025)
+    app.stage.addChild(barre_prout)
+
+    let competences = []
+
+    let arbre = document.createElement("div")
+
+    for (let comp of competences) {
+        let div = document.createElement("div")
+        let titre = document.createElement("h3")
+        titre.innerHTML = competence.name
+        let desc = document.createElement("p")
+        desc.innerHTML = competence.description
+
+        div.appendChild(titre)
+        div.appendChild(desc)
+
+        arbre.appendChild(div)
+    }
 
     // Set scene
     var scene = new HelloWorld(app);
